f840633cc398cc1a22d5700d2e20b6983fa8f175
Yury Shkoda <shkoda.yuri@gmail.com>
2019-06-27 10:06:33 -0400

    1. installed elasticsearch and connected it to our App using mongoosastic. 2. getAllProduct

--- projects/node_e-commerce/routes/index.js
+++ projects/node_e-commerce/routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+let productController = require('./product/controllers/productController')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express'});
+    productController.getAllProducts({})
+                        .then( products => {
+                            res.render('index', {
+                                products: products
+                            })
+                        })
+                        .catch( error => {
+                            res.status(error.status).json(error)
+                        })
 });
 
 module.exports = router;
--- projects/node_e-commerce/routes/product/controllers/productController.js
+++ projects/node_e-commerce/routes/product/controllers/productController.js
@@ -1,1 +1,51 @@
-// create method getAllProducts (use a Promise)
+let Product = require('../models/Product')
+
+module.exports = {
+    getAllProducts: (params) => {
+        return new Promise((resolve, reject) => {
+            Product.find(params)
+                    .then(products => {
+                        resolve(products)
+                    })
+                    .catch( error => {
+                        let errors     = {}
+                        errors.status  = 500
+                        errors.message = error
+
+                        reject(errors)
+                    })
+        })
+    },
+    getProductByID: (id) => {
+        return new Promise((resolve, reject) => {
+            Product.findById(id)
+                    .then(product => {
+                        resolve(product)
+                    })
+                    .catch( error => {
+                        let errors     = {}
+                        errors.status  = 500
+                        errors.message = error
+
+                        reject(errors)
+                    })
+        })
+    },
+    getProductsByCategoryID: (id) => {
+        return new Promise((resolve, reject) => {
+            Product.find({category: id})
+                    .populate('category')
+                    .exec()
+                    .then( products => {
+                        resolve(products)
+                    })
+                    .catch( error => {
+                        let errors = {}
+                        errors.status = 500
+                        errors.message = error
+
+                        reject(errors)
+                    })
+        })
+    }
+}
